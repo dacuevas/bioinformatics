dfb59e197dbc1fc15a38a0ce07de5626fa0417c5
dacuevas <dcuevas08@gmail.com>
2017-07-23 14:24:57 -0700

    Minor refactoring.

--- kegg_api/reactions_to_kegg.py
+++ kegg_api/reactions_to_kegg.py
@@ -4,8 +4,9 @@
 #
 # Author: Daniel A Cuevas (dcuevas08.at.gmail.com)
 # Created on 19 Jul 2017
-# Updated on 19 Jul 2017
+# Updated on 23 Jul 2017
 
+from __future__ import print_function, absolute_import, division
 import requests
 import argparse
 import PyFBA
@@ -17,7 +18,7 @@ import re
 ###############################################################################
 # FUNCTION DEFINITIONS
 ###############################################################################
-def parse_response(response):
+def parse_response(response, mseed, kegg):
     """
     Parse the KEGG API response text. Text is all plain text without an
     easily digestible format. Feature headers are listed at the beginning of
@@ -34,49 +35,82 @@ def parse_response(response):
 
     :param response: API response text
     :type response: str
-    :return: None
+    :param mseed: Model SEED reaction ID
+    :type mseed: str
+    :param kegg: KEGG reaction ID
+    :type kegg: str
+    :return:
     """
+    # Check if response contains all items
+    if not re.search(r'NAME', response):
+        print('No NAME info for', mseed, ':', kegg, file=sys.stderr)
+
+    if not re.search(r'ENZYME', response):
+        print('No ENZYME info for ', mseed, ':', kegg, file=sys.stderr)
+
+    if not re.search(r'ORTHOLOGY', response):
+        print('No ORTHOLOGY info for ', mseed, ':', kegg, file=sys.stderr)
+
     # Response contains newlines
     response = response.split('\n')
 
-    in_orthology = False  # Flag to indicate to obtain all KO values
+    all_lines = []
+    in_orthology = False
     for line in response:
-        if re.match(r'NAME', line):
+        if re.match(r'///', line):
+            # Reached the end of the orthology lines
+            parse_orthology_lines(all_lines)
+            break
+
+        elif re.match(r'NAME', line):
             parse_name_line(line)
 
         elif re.match(r'ENZYME', line):
             parse_enzyme_line(line)
 
-        elif re.match(r'ORTHOLOGY', line) or in_orthology:
+        elif re.match(r'ORTHOLOGY', line):
             in_orthology = True
-            parse_orthology_line(line)
+            ko = line.split()[1]
+            all_lines = [ko]
+
+        elif in_orthology:
+            ko = line.split()[0]
+            all_lines.append(ko)
 
 
 def parse_name_line(line):
     """
+    Parse the NAME line from the KEGG GET response
 
-    :param line:
-    :return:
+    :param line: Name line from the response
+    :type line: str
+    :return: None
     """
-    pass
+    name = re.split(r'\s{2,}', line)[1]
+    print(name + '\t', end='')
 
 
 def parse_enzyme_line(line):
     """
+    Parse the ENZYME line from the KEGG GET response
 
-    :param line:
-    :return:
+    :param line: ENZYME line from the response
+    :type line: str
+    :return: None
     """
-    pass
+    enzymes = line.split()
+    print(';'.join(enzymes[1:]) + '\t', end='')
 
 
-def parse_orthology_line(line):
+def parse_orthology_lines(lines):
     """
+    Parse the ORTHOLOGY (KO) lines from the KEGG GET response
 
-    :param line:
-    :return:
+    :param lines: ORTHOLOGY lines from the response
+    :type lines: list
+    :return: None
     """
-    pass
+    print(';'.join(lines))
 
 
 ###############################################################################
@@ -121,23 +155,27 @@ with open(args.mseed_to_kegg, 'r') as f:
         else:
             mseed_to_kegg[mseed] = kegg
 
-print(str(len(mseed_to_kegg)) + ' reacton IDs found in mapper file',
+print(str(len(mseed_to_kegg)), 'reacton IDs found in mapper file',
       file=sys.stderr)
 
 # Load model
 model = PyFBA.model.load_model(args.model_dir, args.model_name)
+print('Model contains', str(model.number_of_reactions()), 'reactions',
+      file=sys.stderr)
 
 # Set KEGG API URL
 API_BASE_URL = 'http://rest.kegg.jp/'
 
 # Output header
-print('mseed_id\tkegg_id\tec\tpathway')
+print('mseed_id', 'equation', 'kegg_id', 'name', 'ec','pathway', sep='\t')
 
-for mseed_rxn in model.reactions:
+# Iterate through reaction list from model
+for i, mseed_rxn in enumerate(model.reactions, start=1):
+    print('Processing reaction', str(i), end='\r', file=sys.stderr)
     if mseed_rxn in mseed_to_kegg:
         kegg_rxn = mseed_to_kegg[mseed_rxn]
     else:
-        print(mseed_rxn + ' not found in mapper. Skipping.', file=sys.stderr)
+        print(mseed_rxn, 'not found in mapper. Skipping.', file=sys.stderr)
         continue
 
     # Set resource path
@@ -149,15 +187,21 @@ for mseed_rxn in model.reactions:
 
     # Check that status code is 200 = good
     if response.status_code != 200:
-        print('There was an error with the request: status code = ',
+        print('There was an error with the request: status code =',
               response.status_code,
               file=sys.stderr)
-        print('ModelSEED id: ' + mseed_rxn, file=sys.stderr)
-        print('KEGG id: ' + kegg_rxn, file=sys.stderr)
+        print('ModelSEED id:', mseed_rxn, file=sys.stderr)
+        print('KEGG id:', kegg_rxn, file=sys.stderr)
+        print(mseed_rxn, model.reactions[mseed_rxn].equation,
+              kegg_rxn, 'None', 'None', 'None', sep='\t')
         continue
 
     if response.text.strip('\n') == '':
-        print('Response was empty for ' + kegg_rxn, file=sys.stderr)
+        print('Response was empty for', kegg_rxn, file=sys.stderr)
         continue
 
-    parse_response()
+    # Print reaction ID
+    print(mseed_rxn, kegg_rxn, '', end='', sep='\t')
+    parse_response(response.text, mseed_rxn, kegg_rxn)
+
+print('Script completed!', file=sys.stderr)
